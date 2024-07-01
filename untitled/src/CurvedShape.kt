f461f8196f8119831ab67d67cd4d56c6ac15d775
kmerk <mcmillenka@gmail.com>
2024-07-01 12:28:42 -0600

    Changed curved shape implementation slightly

--- untitled/src/CurvedShape.kt
+++ untitled/src/CurvedShape.kt
@@ -1,4 +1,12 @@
-abstract class CurvedShape(origin: Point) : Shape(origin) {
+abstract class CurvedShape(origin: Point, radius1: Double, radius2: Double) : Shape(origin) {
+    abstract val radius1: Double
+    abstract val radius2: Double
+    init {
+        require(radius1 > 0) { println("A curved shape cannot have area 0") }
+        require(radius2 > 0) { println("A curved shape cannot have area 0") }
+    }
     abstract fun getAttributes(): Point
-    abstract override fun getArea(): Double
+    override fun getArea(): Double {
+        return 3.14159 * radius1 * radius2
+    }
 }
--- untitled/src/CurvedShapes/Ellipse.kt
+++ untitled/src/CurvedShapes/Ellipse.kt
@@ -1,15 +1,9 @@
-open class Ellipse(origin: Point, var radius1: Double, var radius2: Double) : CurvedShape(origin) {
-    init {
-        require(radius1 > 0) { println("A curved shape cannot have area 0") }
-        require(radius2 > 0) { println("A curved shape cannot have area 0") }
-    }
+open class Ellipse(origin: Point, override var radius1: Double, override var radius2: Double) : CurvedShape(origin, radius1, radius2) {
+
     override fun getAttributes(): Point {
         println("Origin at (${origin.getX()}, ${origin.getY()})")
         return origin.clone()
     }
 
-    override fun getArea(): Double {
-        return 3.14159 * radius1 * radius2
-    }
 
 }
