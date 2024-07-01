5c7628eee93fcf4446dc46eff12e6b678678cbb6
kmerk <mcmillenka@gmail.com>
2024-07-01 12:24:41 -0600

    Finished implementation and uploaded UML as PDF

--- untitled/src/CurvedShapes/Ellipse.kt
+++ untitled/src/CurvedShapes/Ellipse.kt
@@ -1,4 +1,8 @@
 open class Ellipse(origin: Point, var radius1: Double, var radius2: Double) : CurvedShape(origin) {
+    init {
+        require(radius1 > 0) { println("A curved shape cannot have area 0") }
+        require(radius2 > 0) { println("A curved shape cannot have area 0") }
+    }
     override fun getAttributes(): Point {
         println("Origin at (${origin.getX()}, ${origin.getY()})")
         return origin.clone()
--- untitled/src/Polygons/Square.kt
+++ untitled/src/Polygons/Square.kt
@@ -4,6 +4,4 @@ class Square(origin: Point, width: Double) : Rectangle(origin, width, width) {
     init {
         require(width > 0) { println("A square cannot have 0 width") }
     }
-
-
 }
