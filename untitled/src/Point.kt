754c8563a7de0fd97c11de866ab9e943c90cfad5
kmerk <mcmillenka@gmail.com>
2024-07-01 10:32:18 -0600

    Point implementation

--- untitled/src/Point.kt
+++ untitled/src/Point.kt
@@ -1,2 +1,26 @@
-class Point {
+class Point(private var x: Double, private var y: Double) {
+
+    private fun getX(): Double {
+        return x
+    }
+
+    private fun getY(): Double {
+        return y
+    }
+
+    fun clone(): Point {
+        return Point(x, y)
+    }
+
+    private fun moveX(delta: Double){
+        x += delta
+    }
+    private fun moveY(delta: Double){
+        y += delta
+    }
+
+    fun move(deltaX: Double, deltaY: Double) {
+        moveX(deltaX)
+        moveY(deltaY)
+    }
 }
